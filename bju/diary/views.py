ad1a552bfca02779a188ba29cf7b9aa0e6a5d0c0
Eugene Zolotarev <money230704@gmail.com>
2024-11-24 16:45:00 +0300

    created basic login form with redirecting to home page after loginning

--- bju/diary/views.py
+++ bju/diary/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm
+from django.contrib.auth import authenticate, login as auth_login
 
 
 @login_required
@@ -9,4 +11,22 @@ def index(request):
 
 
 def login(request):
-    return HttpResponse("<p>Страница логина</p>")
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user:
+                auth_login(request, user)
+                # Получаем значение 'next' из параметра запроса
+                next_url = request.GET.get('next')
+                if next_url:  # Если параметр 'next' существует
+                    return redirect(next_url)
+                else:  # Если параметра 'next' нет, перенаправляем на главную страницу дневника
+                    return redirect('diary:index')
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'diary/login.html', context)
